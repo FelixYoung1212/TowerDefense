f70db56af0fc295b0b34b6a512f7bb1afb1e56c5
FeiYang <672466295@qq.com>
2025-12-02 18:06:37 +0800

    新增BehaviourDesigner

--- Assets/ProjectileData.cs
+++ Assets/ProjectileData.cs
@@ -10,5 +10,10 @@ namespace DefaultNamespace
     {
         [Header("飞行物预设")] [SerializeField] private GameObject m_Prefab;
         [Header("飞行物速度")] [SerializeField] private float m_Speed;
+        [Header("飞行物类型")] [SerializeField] private ProjectileType m_Type;
+
+        public ProjectileType Type => m_Type;
+        public GameObject Prefab => m_Prefab;
+        public float Speed => m_Speed;
     }
 }
---
+++ Assets/Settings/CreateProjectile.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DefaultNamespace;
+
+[TaskName("创建发射物")]
+[TaskCategory("创建发射物")]
+public class CreateProjectile : Action
+{
+    [SerializeField] private ProjectileData m_ProjectileData;
+    private Unit m_Unit;
+
+    public override void OnStart()
+    {
+        m_Unit = GetComponent<Unit>();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (m_ProjectileData == null)
+        {
+            Debug.LogError("ProjectileData is not set.");
+            return TaskStatus.Failure;
+        }
+
+        if (m_Unit == null)
+        {
+            Debug.LogError("Unit component is not found.");
+            return TaskStatus.Failure;
+        }
+
+        return TaskStatus.Success;
+    }
+}
